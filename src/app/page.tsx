3250ed0bdd98b4a8a7f3d65b4a3869bafea84004
Nynjin <moncef.hassani03@gmail.com>
2024-06-06 10:40:26 +0200

    feat: dark mode prototype
    added prototype for a dynamic light/dark mode that inverts the color of the components it overlaps
    with

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 'use client'
 
 import useDarkMode from '@/hooks/useDarkMode'
+import { motion } from 'framer-motion'
 import React from 'react'
 
 export default function Home() {
   const [theme, toggleTheme] = useDarkMode()
+  console.log(theme)
 
   return (
-    <div className="bg-white text-black dark:bg-black dark:text-white">
-      <button onClick={toggleTheme}>Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode</button>
-    </div>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <motion.div
+        className="bg-white text-black dark:bg-black dark:text-white"
+        onClick={toggleTheme}
+        initial={{ scale: 1 }}
+        whileHover={{ scale: 3 }}
+      >
+        Toggle Theme
+      </motion.div>
+
+      <p className="mix-blend-difference text-white">Hello World</p>
+    </motion.div>
   )
 }
